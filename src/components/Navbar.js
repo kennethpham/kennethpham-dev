56232e17970e9173f55552b9d9973b8f0613c334
Kenneth Pham <kennethpham98@gmail.com>
2021-04-28 20:52:29 -0700

    changed navbar buttons to material-ui and added static images to them

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,36 @@
 import '../styles/Navbar.css';
+import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  root: {
+    maxHeight: "35px",
+    minWidth: "35px",
+    minHeight: "30px"
+  }
+})
+
 export default function Navbar() {
+  const buttonStyle = useStyles();
   return(
     <div className="navbar">
-        <Button variant="contained">Github</Button>
-        <Button variant="contained">LinkedIn</Button>
-        <Button variant="contained">Projects</Button>
+        <div className="navButton">
+          <Button variant="contained" className={buttonStyle.root}>
+            <img src={`${process.env.PUBLIC_URL}/images/Octicons-mark-github.svg`}
+             alt="Github" height="25" width="25" />
+          </Button>
+        </div>
+        <div className="navButton">
+          <Button variant="contained" className={buttonStyle.root}>
+            <img src={`${process.env.PUBLIC_URL}/images/linkedIn.png`}
+              alt="LinkedIn" height="30" width="30" />
+          </Button>
+        </div>
+        <div className="navButton">
+          <Button variant="contained" className={buttonStyle.root}>
+            Projects
+          </Button>
+        </div>
     </div>
   );
 }
