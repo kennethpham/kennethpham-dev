fa07941c2c2b1ee88fa886436d7ba79fbd153042
Kenneth Pham <kennethpham98@gmail.com>
2025-04-30 23:41:25 -0700

    change to vite with tailwind and shadcn
    Currently removes use of react-router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
-import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
+      <Homepage />
     </div>
   );
 }
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,20 +1,18 @@
 import '../styles/Homepage.css';
-import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
+import { Button } from '@mui/material';
 import { useEffect } from 'react';
 
-const useStyles = makeStyles({
-  root: {
-    maxHeight: '35px',
-    minWidth: '35px',
-    minHeight: '30px',
-    textTransform: 'none'
-  }
-})
+// const useStyles = makeStyles({
+//   root: {
+//     maxHeight: '35px',
+//     minWidth: '35px',
+//     minHeight: '30px',
+//     textTransform: 'none'
+//   }
+// })
 
 export default function Homepage() {
-  const buttonStyle = useStyles();
+  // const buttonStyle = useStyles();
   useEffect(()=>{
     document.title = 'Kenneth\'s Website';
     document.body.style.backgroundColor = '#FFEFD5';
@@ -23,25 +21,18 @@ export default function Homepage() {
     <div className='homepage'>
       <div className='navbar'>
           <div className='navButton'>
-            <Button variant='contained' className={buttonStyle.root}
+            <Button variant='contained'
               href='https://github.com/kennethpham'>
-              <img src={`${process.env.PUBLIC_URL}/images/Octicons-mark-github.svg`}
+              <img src={`/images/Octicons-mark-github.svg`}
               alt='Github' height='25' width='25' />
             </Button>
           </div>
           <div className='navButton'>
-            <Button variant='contained' className={buttonStyle.root}
+            <Button variant='contained'
               href='https://www.linkedin.com/in/kenneth-pham-246a90183/'>
-              <img src={`${process.env.PUBLIC_URL}/images/linkedIn.png`}
+              <img src={`/images/linkedIn.png`}
                 alt='LinkedIn' height='30' width='30' />
             </Button>
-          </div>
-          <div className='navButton'>
-            <Link to='/projects'>
-              <Button variant='contained' className={buttonStyle.root}>
-                <p>Projects</p>
-              </Button>
-            </Link>
           </div>
       </div>
       <div className='header'>
